87b49a4132d58ad7229b45bb3b9ef44d15dac194
Rodrigo de León <rodrigo.deleon@ciberc.com>
2023-01-30 17:46:05 -0600

    try initial cert compose

--- app/proj/settings.py
+++ app/proj/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-8*_3vnk&!wbgzix33%q*4ei)botkod&isy7xvvv%9$xbc&al4g'
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
