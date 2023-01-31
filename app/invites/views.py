57ed7f55cc2d3155c9cdb594a5eeba9658cb127e
Rodrigo de León <rodrigo.deleon@ciberc.com>
2023-01-30 22:18:30 -0600

    simplify and fix form

--- app/invites/views.py
+++ app/invites/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpRequest, HttpResponse
 from django.views import View
@@ -24,15 +24,11 @@ class ConfirmInvitationView(View):
                       context)
 
     def post(self, request: HttpRequest, uuid: str):
-        total_confirmed = int(
-            request.POST.get('total_confirmed')
-        )
         invite = Invite.objects.get(id=uuid)
-        invite.total_confirmed = total_confirmed
         invite.confirmed = True
         invite.confirm_date = timezone.now()
         invite.save(force_update=True)
-        return HttpResponse('Actualizado')
+        return redirect('invites:confirm', invite.id)
 
 
 class DeclineInvitation(View):
@@ -41,22 +37,15 @@ class DeclineInvitation(View):
         invite.confirmed = False
         invite.confirm_date = timezone.now()
         invite.save(force_update=True)
-        return HttpResponse('No asistiré')
-
-
-class ConfirmCompletedView(View):
-    pass
+        return redirect('invites:confirm', invite.id)
 
 
 class ListInvitationView(View):
     def get(self, request: HttpRequest):
-        # print(request.META)
-        # breakpoint()
         URL_BASE = (
             f"{request.META['wsgi.url_scheme']}://"
             f"{request.META['HTTP_HOST']}"
         )
-        print(URL_BASE)
         uris = []
         invites = Invite.objects.all()
         for invite in invites:
@@ -65,7 +54,6 @@ class ListInvitationView(View):
             )
             uri = f"{URL_BASE}{relative}"
             uris.append({'name': invite.name, 'url': uri})
-        print(relative)
         context = {'invite_urls': uris}
         return render(request, 'invites/list.html', context)
         # return HttpResponse(f'List: {uris}')
